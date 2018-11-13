2de81973bf1ec2c7e195c446ce5ed5578f97a87b
Keoni Murray <keonimurray45@gmail.com>
2018-11-13 14:41:25 -0800

    new repo

---
+++ hero.py
@@ -0,0 +1,84 @@
+import random
+
+
+class Hero:
+    def __init__(self, name, current_health, starting_health =100, ):
+        self.name = name 
+        self.abilities = list()
+        self.starting_health = starting_health 
+        self.current_health = starting_health 
+    
+    def add_ability(self, ability):
+        self.abilities.append(self.ability)
+    
+    def attack(self):
+        #will get back to this function 
+        for hero_attack in self.abilities:
+            return attack()
+    
+    def take_damage(self, damage):
+        self.current_health -= damage
+    
+    def is_alieve(self):
+        if self.current_health >= 0:
+            return True
+            print('hero is alieve')
+        else:
+            return false
+            print('hero is dead')
+    
+    def fight(self, opponent):
+       while self.is_alieve() and opponent.is_alieve():
+           damage = self.attack()
+           opponent.take_damage(damage)
+
+
+
+
+class Ability:
+    
+    def __init__(self, name, attack_strength):
+        self.name = name 
+        self.attack_strength = attack_strength
+    def attack(self):
+        self.attack_strength = random.randint(0, 10)
+
+class Weapon(Ability):
+    def attack(self):
+        
+        return self.attack() // self.attack_strength
+
+
+class Team:
+    def __init__(self, team_name):
+        self.name = name
+        self.heroes = list()
+    
+    def add_hero(self, Hero):
+        self.heroes.append(Hero)
+    
+    def remove_hero(self, name):
+        self.heroes.remove(name)
+    
+    def view_all_heroes(self):
+
+        for i in self.heroes:
+            print(i)
+    
+    
+
+
+
+
+if __name__ == '__main__':
+    hero = Hero("Wonder Woman")
+    print(hero.attack())
+    ability = Ability("Divine Speed", 300)
+    hero.add_ability(ability)
+    print(hero.attack())
+    new_ability = Ability("Super Human Strength", 800)
+    hero.add_ability(new_ability)
+    print(hero.attack())
+
+
+        
